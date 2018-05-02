7a2918703632df3f3d0446fdd032e1045242c282
Takuya Horimatsu <new_woory@hotmail.com>
2018-05-02 17:30:53 +0900

    show help/error message

--- cli.c
+++ cli.c
@@ -1,5 +1,10 @@
 #include "jpeg.c"
 
+#define HelpMessage "Usage: %s [input] [output]\n" \
+            "  -h\tshow usage\n" \
+            " Convert option:\n" \
+            "  -gs\tto gray scale\n" \
+
 /* Takes a bmp image specified at the command line and
  * encodes it in JPEG format and writes to another file
  * specified at the command line */
@@ -7,6 +12,17 @@ int main(int argc, char *argv[])
 {
     /* Command line switch for color and greyscale images */
     int num_colors = 3;
+
+    if (argc == 2 && strcmp(argv[1], "-h") == 0) {
+        printf(HelpMessage, argv[0]);
+        return 0;
+    }
+    if (argc < 3) {
+        fprintf(stderr, "argment error\n");
+        printf(HelpMessage, argv[0]);
+        return 1;
+    }
+
     if (argc == 4 && strcmp(argv[3], "-gs") == 0)
         num_colors = 1;
 
